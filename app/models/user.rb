1e460cc5531978e6d2f8a6a5ec6b9572d8dd32d5
Huemac Alfredo Córdova <hmca101292@outlook.com>
2021-02-07 03:40:27 -0600

    configreate rails routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
